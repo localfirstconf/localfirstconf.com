40d71fa84032a70042d474c39c44334fcead32b7
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-05-17 18:39:56 +0200

    Fix react/no-unescaped-entities in schedule-page.tsx

--- src/components/schedule-page.tsx
+++ src/components/schedule-page.tsx
@@ -20,10 +20,12 @@ export const SchedulePage: FC<{sessions: (Omit<Session, 'speaker'> & {speaker: S
       <div className="mb-1 uppercase tracking-widest text-orange md:text-lg">Local-First Conf</div>
       <h1 className="font-display text-4xl uppercase leading-none md:text-7xl">Conference Day Schedule</h1>
       <p className="mb-8 mt-8">
-        The main day is on May 30, 2024. The venue is Schankhalle Pfefferberg located at <a href="https://maps.app.goo.gl/sFm1EMsFPkQAHt1b7">Schönhauser Allee 176, 10119 Berlin</a>.
+        The main day is on May 30, 2024. The venue is Schankhalle Pfefferberg located at{' '}
+        <a href="https://maps.app.goo.gl/sFm1EMsFPkQAHt1b7">Schönhauser Allee 176, 10119 Berlin</a>.
       </p>
       <p className="mb-16">
-        The expo day is on May 31, 2024. The venue is Gitbutler's offices, located at <a href="https://maps.app.goo.gl/xCNcnbg29AhsCwmv7">Schönhauser Allee 43a/44, 10435 Berlin</a>.
+        The expo day is on May 31, 2024. The venue is Gitbutler&apos;s offices, located at{' '}
+        <a href="https://maps.app.goo.gl/xCNcnbg29AhsCwmv7">Schönhauser Allee 43a/44, 10435 Berlin</a>.
       </p>
       <Schedule sessions={sessions} />
     </div>
