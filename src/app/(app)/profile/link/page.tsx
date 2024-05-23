a881fb6c0cb3f011467c324338db8c82a86fc335
Lukas Wiesehan <mail@lukaswiesehan.de>
2024-05-23 09:04:50 +0200

    Add combobox

--- src/app/(app)/profile/link/page.tsx
+++ src/app/(app)/profile/link/page.tsx
@@ -1,45 +1,63 @@
 'use client'
 
+import {Combobox, ComboboxInput, ComboboxOption, ComboboxOptions} from '@headlessui/react'
+import {CheckIcon} from '@heroicons/react/20/solid'
+import {allProfiles} from 'contentlayer/generated'
 import Link from 'next/link'
 import {useRouter} from 'next/navigation'
-import {FormEventHandler, useState} from 'react'
+import {useState} from 'react'
+
+const profiles = allProfiles.map((profile) => ({slug: profile.slug, name: profile.name}))
 
 export default function LinkAttendeePage() {
-  const router = useRouter()
-  const [slug, setSlug] = useState<string>('')
+  const [selectedPerson, setSelectedPerson] = useState()
+  const [query, setQuery] = useState('')
 
-  const linkProfile: FormEventHandler = (e) => {
-    e.preventDefault()
-    if (slug) localStorage.setItem('attendee-slug', slug)
-    router.push(`/profile/${slug}`)
-  }
+  const filteredPeople =
+    query === ''
+      ? profiles
+      : profiles.filter((profile) => {
+          return profile.name.toLowerCase().includes(query.toLowerCase())
+        })
 
   return (
     <div className="w-full max-w-3xl gap-8 px-4 py-24 md:px-0">
       <h1 className="font-display text-4xl uppercase leading-none">Link your profile</h1>
-      <div className="max-w-lg">
-        <p className="my-8">
-          To link your attendee profile here, enter your slug. If you don&apos;t have one, create it{' '}
-          <Link href="https://github.com/localfirstconf/localfirstconf.com/tree/main/content/profiles#readme" className="underline">
-            here
-          </Link>
-          .
-        </p>
-        <form className="flex h-12 gap-2 rounded-full border border-white p-1 focus-within:ring focus-within:ring-white/40" onSubmit={linkProfile}>
-          <input
-            type="text"
-            required
-            name="slug"
-            placeholder="Your slug"
-            value={slug}
-            onChange={(e) => setSlug(e.target.value)}
-            className="h-full shrink grow rounded-full bg-transparent px-4 text-white placeholder-neutral-500 focus:outline-none"
+      <p className="my-8 max-w-lg">
+        To link your attendee profile here, enter your slug. If you don&apos;t have one, create it{' '}
+        <Link
+          href="https://github.com/localfirstconf/localfirstconf.com/tree/main/content/profiles#readme"
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          here
+        </Link>
+        . Your name will appear here after we merge your PR.
+      </p>
+      <div className="flex gap-2">
+        {/* @ts-ignore */}
+        <Combobox value={selectedPerson} onChange={setSelectedPerson} onClose={() => setQuery('')}>
+          <ComboboxInput
+            aria-label="Profile"
+            displayValue={(person: any) => person?.name}
+            onChange={(event) => setQuery(event.target.value)}
+            className="h-12 w-64 rounded-full border border-white bg-black px-6 text-white"
           />
-          <button type="submit" className="h-full shrink-0 rounded-full bg-magenta px-4 text-black hover:bg-white focus:bg-white focus:outline-none">
-            <span>Link</span>
-            <span className="hidden md:inline"> profile</span>
-          </button>
-        </form>
+          <ComboboxOptions anchor="bottom" className="mt-2 w-64 rounded-3xl border border-white bg-black p-1 empty:hidden">
+            {filteredPeople.map((person) => (
+              <ComboboxOption
+                key={person.slug}
+                value={person}
+                className="group flex h-10 items-center gap-2 rounded-full px-4 data-[focus]:bg-neutral-700"
+              >
+                <span>{person.name}</span>
+                <CheckIcon className="invisible size-5 group-data-[selected]:visible" />
+              </ComboboxOption>
+            ))}
+          </ComboboxOptions>
+        </Combobox>
+        <button className="h-12 rounded-full bg-magenta px-6 text-black">Link profile</button>
       </div>
     </div>
   )
